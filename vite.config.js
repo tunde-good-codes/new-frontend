25bd1630707d1db9e67e03a5a040f3c72d7a59d6
tundenewcodes <fadipe11@gmail.com>
2025-06-21 19:53:15 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+//import mkcert from "vite-plugin-mkcert";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), 
+    
+    // mkcert()
+  
+  ],
+  server: {
+    https: false,
+    host: "localhost",
+    port: 5173,
+  },
+});
